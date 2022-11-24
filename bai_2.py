e81481f2c47ae5f2557c9bb433b2b1b76ca141f9
TrangVo <118703634+Trang2111-hub@users.noreply.github.com>
2022-11-24 22:20:20 +0700

    Update bai_2.py

--- bai_2.py
+++ bai_2.py
@@ -1,28 +1,20 @@
-a = int(input("nhập số hạng A: "))
-b = int(input("nhập số hạng B: "))
-c = 0
-d = 0
-lista=[]
-listb=[]
-n=0
-m=0
-ucln=0
-print("Các ước của A là: ")
-for i in range(1,a+1):
-    c += 1
-    if a % c == 0:
-       lista.insert(n,c)
-       n=n+1
-print("Các ước của B là: ")
-for i in range(1,b+1):
-    d += 1
-    if b % d == 0:
-       listb.insert(m,d)
-       m=m+1
-print(lista)
-print(listb)
-giao_nghiem = set(lista)&set(listb)
-for i in giao_nghiem:
-    if i > ucln:
-        ucln = i
-print("Ước chung lớn nhất: ",ucln)
+import math
+s=0
+a=float(input("Nhập a="))
+b=float(input("Nhập b="))
+c=float(input("Nhập c="))
+a1= float(input("Nhập a1="))
+b1 = float(input("Nhập b1="))
+c1 = float(input("Nhập c1="))
+delta=b**2 - 4*a*c
+delta1=b1**2 - 4*a1*c1
+if (delta>0):
+   x1 = float((-b + math.sqrt(delta)) / (2 * a))
+   x2 = float((-b - math.sqrt(delta)) / (2 * a))
+if (delta1>0):
+   x3 = float((-b1 + math.sqrt(delta1)) / (2 * a1))
+   x4 = float((-b1 - math.sqrt(delta1)) / (2 * a1))
+s=x1+x2+x3+x4
+print ('Tổng các nghiệm là: ',s)
+ 
+

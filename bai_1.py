a8b0d25bdb057920ed9cda3075aa784e6209f5a9
TrangVo <118703634+Trang2111-hub@users.noreply.github.com>
2022-11-24 21:56:53 +0700

    Update bai_1.py

---
+++ bai_1.py
@@ -0,0 +1,12 @@
+a=int(input(" Nhập a : "))
+b=int(input(" Nhập b : "))
+ucln = 1
+if (a<b):
+ min=a
+else:
+  min=b
+for i in range (1,min+1):
+  if (a%i == 0):
+    if (b%i==0):
+     ucln=i
+print('Ước chung lớn nhất là: 'ucln)

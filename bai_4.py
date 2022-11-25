c38fed34ccf256a7ae41ce23c34c159e3db90ad6
TrangVo <118703634+Trang2111-hub@users.noreply.github.com>
2022-11-25 08:49:11 +0700

    Update bai_4.py

--- bai_4.py
+++ bai_4.py
@@ -2,9 +2,12 @@ n= int(input('Nhập n: '))
 f1= 0
 f2= 1
 fn= 0
+if n==1:
+    fn=0
+elif n==2:
+    fn=1
 for i in range(3, n+1):
     fn = f1 + f2
     f1 = f2
     f2 = fn
-print("số hạng thứ", n, "của dãy fibonaci là:", fn)
- 
+print("Số hạng thứ", n, "của dãy fibonaci là:", fn)
